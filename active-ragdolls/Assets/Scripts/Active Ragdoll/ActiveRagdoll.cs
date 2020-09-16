393b7a4abdb73c022faab49f4dd8ec3c5f832f77
Sergio Abreu García <saggamedeveloper@gmail.com>
2020-09-16 17:50:04 +0200

    Physical body following animated movements, basic.

--- active-ragdolls/Assets/Scripts/Active Ragdoll/ActiveRagdoll.cs
+++ active-ragdolls/Assets/Scripts/Active Ragdoll/ActiveRagdoll.cs
@@ -2,12 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActiveRagdoll : MonoBehaviour {
-    [Header("Body")]
-    [SerializeField] private Transform _animatedTorso;
-    [SerializeField] private Rigidbody _physicalTorso;
+namespace ActiveRagdoll {
+    public class ActiveRagdoll : MonoBehaviour {
+        [Header("Body")]
+        [SerializeField] private Transform _animatedTorso;
+        [SerializeField] private Rigidbody _physicalTorso;
 
-    [Header("Advanced")]
-    [SerializeField] private int _solverIterations = 11;
-    [SerializeField] private int _velSolverIterations = 11;
+        private Transform[] _animatedBody;
+        private ConfigurableJoint[] _joints;
+        private Quaternion[] _initialJointsRotation;
+
+        [Header("Advanced")]
+        [SerializeField] private int _solverIterations = 11;
+        [SerializeField] private int _velSolverIterations = 11;
+
+        void Start() {
+            _animatedBody = _animatedTorso.GetComponentsInChildren<Transform>();
+            _joints = _physicalTorso.GetComponentsInChildren<ConfigurableJoint>();
+
+            _initialJointsRotation = new Quaternion[_joints.Length];
+            for (int i = 0; i < _joints.Length; i++)
+                _initialJointsRotation[i] = _joints[i].transform.localRotation;
+        }
+
+        void FixedUpdate() {
+            for (int i = 0; i < _joints.Length; i++) {
+                ConfigurableJointExtensions.SetTargetRotationLocal(_joints[i], _animatedBody[i + 1].localRotation, _initialJointsRotation[i]);
+            }
+        }
+    }
 }
