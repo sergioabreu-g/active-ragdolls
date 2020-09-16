281f7ed28ad4c4199c09f787a30872a9a9b39298
Sergio Abreu García <saggamedeveloper@gmail.com>
2020-09-16 17:25:48 +0200

    Basic body: rigidbodies, colliders & joints.

---
+++ active-ragdolls/Assets/Scripts/Active Ragdoll/ActiveRagdoll.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveRagdoll : MonoBehaviour {
+    [Header("Body")]
+    [SerializeField] private Transform _animatedTorso;
+    [SerializeField] private Rigidbody _physicalTorso;
+
+    [Header("Advanced")]
+    [SerializeField] private int _solverIterations = 11;
+    [SerializeField] private int _velSolverIterations = 11;
+}
